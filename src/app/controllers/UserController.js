d53b2c54379679cbee0227e4bd5e0a5919acb1e2
Lucas Machado <lucasvufma@gmail.com>
2020-04-08 20:53:22 -0300

    feat: add user controller

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,66 @@
+import User from '../models/User';
+
+class UserController {
+  async store(req, res) {
+    const { id, name, email, status } = await User.create(req.body);
+
+    return res.status(200).json({
+      id,
+      name,
+      email,
+      status,
+    });
+  }
+
+  async update(req, res) {
+    const user = await User.findByPk(req.userId);
+    if (!user) {
+      return res.status(404).json({
+        error: { message: "Can't find requested user, maybe dosn't exists" },
+      });
+    }
+    const { oldPassword } = req.body;
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Password not match!' });
+    }
+
+    const { id, name, email } = await user.update(req.body);
+
+    return res.status(200).json({
+      id,
+      name,
+      email,
+    });
+  }
+
+  async show(req, res) {
+    const user = await User.findByPk(req.userId);
+    if (!user) {
+      return res.status(404).json({
+        error: { message: "Can't find requested user, maybe dosn't exists" },
+      });
+    }
+
+    return res.status(200).json({
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      status: user.status,
+    });
+  }
+
+  async delete(req, res) {
+    const user = await User.findByPk(req.userId);
+    if (!user) {
+      return res.status(404).json({
+        error: { message: "Can't find requested user, maybe dosn't exists" },
+      });
+    }
+    await user.delete();
+
+    return res.status(204);
+  }
+}
+
+export default new UserController();
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,11 @@
 import { Router } from 'express';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get()('/', (req, res) => res.json({ hello: 'Hello world!' }));
+routes.post('/users', UserController.store);
+routes.get('/users/:id', UserController.show);
+routes.put('/users/:id', UserController.update);
+routes.delete('/users/:id', UserController.delete);
 
 module.exports = routes;
